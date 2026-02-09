5a8cb4fc4ec09705972743ceaef75ceba9959713
bobrakovpasa <bobrakovpasa@gmail.com>
2026-02-09 15:58:47 +0700

    Add debug monitoring script with logging, HTML dumps, and diagnostic tests.

--- debug_monitor.py
+++ debug_monitor.py
@@ -1,17 +1,45 @@
-# Debug Monitor Script
-
-import datetime
 import logging
+import os
+import json
+import datetime
 
-# Set up logging configuration
-logging.basicConfig(level=logging.DEBUG, filename='debug_monitor.log',
-                    format='%(asctime)s - %(levelname)s - %(message)s')
+# Set up logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s', filename='debug_monitor.log', filemode='a')
 
-# Debugging function
+class DebugMonitor:
+    def __init__(self):
+        logging.info('Initializing DebugMonitor')
 
-def debug_monitor():
-    current_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    logging.debug(f'Current Date and Time (UTC): {current_time}')
+    def log_debug_info(self, info):
+        logging.debug(info)
+
+    def dump_html(self, data, filename='dump.html'):
+        logging.info(f'Dumping data to {filename}')
+        with open(filename, 'w') as f:
+            f.write('<html><body><h1>Debug Dump</h1>')
+            f.write('<pre>{}</pre>'.format(json.dumps(data, indent=4)))
+            f.write('</body></html>')
+
+    def perform_diagnostic_tests(self):
+        logging.info('Performing diagnostic tests')
+        # Add diagnostic tests here
+        # Example test for detecting battles
+        battles_detected = self.detect_battles()  # Placeholder for actual battle detection logic
+        if battles_detected:
+            logging.info('Battles detected!')
+        else:
+            logging.info('No battles detected.')
+
+    def detect_battles(self):
+        # Placeholder for real detection logic
+        # For example, you could check specific conditions in your environment
+        # Return True if battles are detected, otherwise False
+        return False  # Modify this according to your logic
 
 if __name__ == '__main__':
-    debug_monitor()
+    debug_monitor = DebugMonitor()
+    debug_monitor.log_debug_info('Debug monitoring started')
+    # Simulate some data to dump
+    sample_data = {'status': 'ok', 'timestamp': datetime.datetime.now().isoformat()}
+    debug_monitor.dump_html(sample_data)
+    debug_monitor.perform_diagnostic_tests()
